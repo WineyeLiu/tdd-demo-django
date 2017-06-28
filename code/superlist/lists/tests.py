966e29bdadf19943c42895adaf683c420c0a0307
wineye liu <mistwineye@hotmail.com>
2017-06-28 14:14:25 +0800

    add first view and return a simple html

--- code/superlist/lists/tests.py
+++ code/superlist/lists/tests.py
@@ -1,7 +1,23 @@
 from django.test import TestCase
+from django.core.urlresolvers import resolve
+from lists.views import home_page
+from django.http import HttpRequest
 
 # Create your tests here.
-
+'''
 class SmokeTest(TestCase):
     def test_haha(self):
         self.assertEqual(2, 3)
+
+'''
+class HomePageTest(TestCase):
+    def test_root_url_resolve(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home_page) 
+    
+    def test_return_html(self):
+        request = HttpRequest()
+        response = home_page()
+        self.assertTrue(response.content.startswith(b'<html>'))
+        self.assertIn(b'<title>To-Do lists</title>', response.content)
+        self.assertTrue(response.content.endswith(b'</html>'))
